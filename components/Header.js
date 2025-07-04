d3502c9af1bea0cb37117064d9793242f69cc176
Narsi Jangid <149683470+narsijangid@users.noreply.github.com>
2025-07-04 17:25:36 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <header className="mx-auto flex max-w-screen-xl items-center px-3 py-2.5 2xl:max-w-screen-2xl">
             <Link href={'/'} className="mr-auto">
                 <div className="relative">
-                    <span className="relative z-10 bg-gradient-to-r from-blue-500 via-blue-600 to-purple-600 bg-clip-text text-3xl font-bold tracking-tight text-transparent">
+                    <span className="relative z-10 bg-gradient-to-r from-blue-500 via-blue-600 to-purple-600 >
                         ATS PRO
                     </span>
                     <div className="absolute inset-0 overflow-hidden">
